2d946e0bb5bcc2961c6078d412f76a94be455432
fdgongora2 <fdgongora@iesmurgi.org>
2021-11-01 18:24:40 +0100

    Añadidas las dependencias org.mariadb.jdbc 2.6

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.javafx_acceso_bbdd_jdbc {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.javafx_acceso_bbdd_jdbc to javafx.fxml;
